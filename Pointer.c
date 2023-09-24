f4b402d12985fd604fc1b39d5b5304aa162bb7d7
Wahyu Mentari Mayadita <140961917+whymydt@users.noreply.github.com>
2023-09-24 23:48:33 +0700

    Update Pointer.c

--- Pointer.c
+++ Pointer.c
@@ -3,22 +3,17 @@
 
 int main()
 {
-  char nama1[20];  // Deklarasi array character
-  char *nama2;     // Deklarasi pointer character
-  char *nama3[20]; // Deklarasi array of 20 pointer character
+  char nama1[20];
+  char *nama2;
+  char *nama3[20];
+  
+  strcpy(nama1, "Brisia Jodie"); // Assign string variable nama1
 
-  // Mengisikan data string ke nama1
-  strcpy(nama1, "Brisia Jodie");
+  nama2 = "Brisia Jodie"; // Assign string variable nama2
 
-  // Mengisikan alamat data literal string ke nama2
-  nama2 = "Brisia Jodie";
-
-  // Mengisikan alamat data literal string ke nama3[0]
-  nama3[0] = "Brisia Jodie";
-  printf("%s\n%s\n%s", nama1, nama2, nama3[0]);
+  nama3[0] = "Brisia Jodie"; // Assign string variable nama3[0]
+  
+  printf("%s %s %s", nama1, nama2, nama3[0]); // Output berupa string pada variable nama1, nama2 dan nama3;
+  
   return 0;
-
-  /**
-   * Penting untuk diingat bahwa dalam kasus nama2 dan nama3, program tersebut mengisikan alamat memori dari string literal ke pointer tersebut. Dalam kasus nama1, program tersebut menyalin string literal ke dalam array karakter.
-  */
 }
