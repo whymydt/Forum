ee172ac02b68c6fafb3a68563f7677a953cbe8be
Wahyu Mentari Mayadita <140961917+whymydt@users.noreply.github.com>
2023-10-15 23:33:35 +0700

    Update Menggunakan Array of Pointer of Dynamic Structure.c

--- Menggunakan Array of Pointer of Dynamic Structure.c
+++ Menggunakan Array of Pointer of Dynamic Structure.c
@@ -1,49 +1,47 @@
 #include <stdio.h>
 
-// Membuat tipe data struct untuk data berkelompok yang tipe datanya berbeda
-struct Mahasiswa
+// Deklarasi struct
+typedef struct
 {
-    int id;
-    char nama[50];
-    char prodi[50];
-    float gpa;
-};
+    int ID;
+    char nama[100];
+    char prodi[100];
+    float GPA;
+} mahasiswa;
 
 int main()
 {
-    struct Mahasiswa mahasiswa; // Deklarasi variabel "mahasiswa" dengan tipe data struct
+    mahasiswa mhs; // Deklarasi variabel untuk struct mahasiswa
 
-    int pilihan = 0; // Deklarasi dan inisialisasi
+    int option = 0; 
 
-    while (pilihan != 3)
-    {
-        // Saya berikan warna hijau di tampilannya
+    while (option != 3)
+        
         printf("\n\033[1;32m==============================================\033[0m\n");
-        printf("\033[1;32m================ MENU PILIHAN ================\033[0m\n");
+        printf("\033[1;32m----------------- MENU -----------------\033[0m\n");
         printf("\033[1;32m==============================================\033[0m\n");
 
-        printf("1. Input Data Mahasiswa\n");
-        printf("2. Tampilkan Data Mahasiswa\n");
-        printf("3. Keluar\n");
-        printf("Pilih Menu: ");
-        scanf("%d", &pilihan);
+        printf("1. Input Data \n");
+        printf("2. Tampilkan Data\n");
+        printf("3. Exit Program\n");
+        printf("Pilih menu: ");
+        scanf("%d", &option);
 
         if (pilihan == 1)
         {
             // Meminta input dari user
-            printf("Masukkan ID     : ");
-            scanf("%d", &mahasiswa.id);
-            printf("Masukkan Nama   : ");
-            scanf(" %[^\n]", mahasiswa.nama);
-            printf("Masukkan Prodi  : ");
-            scanf(" %[^\n]", mahasiswa.prodi);
-            printf("Masukkan GPA    : ");
-            scanf("%f", &mahasiswa.gpa);
+            printf("Input ID     : ");
+            scanf("%d", &mhs.id);
+            printf("Input Nama   : ");
+            scanf(" %[^\n]", mhs.nama);
+            printf("Input Prodi  : ");
+            scanf(" %[^\n]", mhs.prodi);
+            printf("Input GPA    : ");
+            scanf("%f", &mhs.gpa);
         }
-        else if (pilihan == 2)
+        else if (option == 2)
         {
-            // Tampilkan data yang telah diinputkan
-            if (mahasiswa.id != 0)
+            if (mhs.id != 0)
             {
                 printf("\nData Mahasiswa:\n");
                 printf("ID              : %d\n", mahasiswa.id);
@@ -51,26 +49,17 @@ int main()
                 printf("Prodi           : %s\n", mahasiswa.prodi);
                 printf("GPA             : %.2f\n", mahasiswa.gpa);
             }
-            else
-            {
-                // Pemberitahuannya saya beri warna merah
-                printf("\033[1;31m==============================================\033[0m\n");
-                printf("\033[1;31m== Belum Ada Data Mahasiswa yang Diinputkan ==\033[0m\n");
-                printf("\033[1;31m==============================================\033[0m\n");
-            }
         }
-        else if (pilihan != 3)
+        else if (option != 3)
         {
-            // Pemberitahuannya saya beri warna merah
             printf("\033[1;31m==============================================\033[0m\n");
-            printf("\033[1;31m== Pilihan Tidak Valid (Pilih 1, 2, atau 3) ==\033[0m\n");
+            printf("\033[1;31m-- Input Invalid (Pilih 1, 2, atau 3) --033[0m\n");
             printf("\033[1;31m==============================================\033[0m\n");
         }
     }
 
-    // Akhir program saya beri warna hijau
     printf("\033[1;32m==============================================\033[0m\n");
-    printf("\033[1;32m= Terima Kasih telah Menggunakan Program Ini =\033[0m\n");
+    printf("\033[1;32m= Program Exited. =\033[0m\n");
     printf("\033[1;32m==============================================\033[0m\n\n");
 
     return 0;
